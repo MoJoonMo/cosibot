5471bfcb372c7ee9fc0c3acb943ca4b4e1a52cd2
mojoonmo <slayerzone8@gmail.com>
2023-12-19 16:32:27 +0900

    gpt 기능 추가

--- index.js
+++ index.js
@@ -12,12 +12,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
+const openai_1 = __importDefault(require("openai"));
 const core_1 = require("@remote-kakao/core");
-const app = (0, express_1.default)();
-const port = 5000;
+// ChatGPT 시작
+const openai = new openai_1.default();
+function gpt_question(p_role, p_content) {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log(p_role);
+        console.log(p_content);
+        const completion = yield openai.chat.completions.create({
+            messages: [{ role: p_role, content: p_content }],
+            model: "gpt-3.5-turbo",
+        });
+        console.log(completion);
+        return (completion.choices[0].message.content);
+    });
+}
+//ChatGPT 끝
 // remote kakao 설정 시작
-const prefix = ">";
+const prefix = "!";
 const server = new core_1.UDPServer({ serviceName: "Example Service" });
 server.on("message", (msg) => __awaiter(void 0, void 0, void 0, function* () {
     var _a;
@@ -33,6 +46,20 @@ server.on("message", (msg) => __awaiter(void 0, void 0, void 0, function* () {
         const timestamp = Date.now();
         yield msg.replyText("Pong!");
         msg.replyText(`${Date.now() - timestamp}ms`);
+    }
+    else if (cmd === "gpt") {
+        const p_role = "system";
+        let p_content = "";
+        if (args.length >= 1) {
+            p_content = msg.content.split(prefix.concat(cmd))[1];
+            const rep = yield gpt_question(p_role, p_content);
+            console.log(msg);
+            console.log(rep);
+            msg.replyText(msg.sender.name + "님의 질문\nQ. " + p_content + "\n\n" + rep);
+        }
+        else {
+            msg.replyText("질문이 없습니다.");
+        }
     }
 }));
 server.start();
--- index.ts
+++ index.ts
@@ -1,11 +1,24 @@
-import express, { Express, Request, Response } from 'express';
+import OpenAI from "openai";
+
 import { UDPServer } from "@remote-kakao/core";
 
-const app: Express = express();
-const port = 5000;
+// ChatGPT 시작
+const openai = new OpenAI();
+
+async function gpt_question(p_role : any, p_content : any) {
+  console.log(p_role);
+  console.log(p_content);
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: p_role, content: p_content }],
+    model: "gpt-3.5-turbo",
+  });
+  console.log(completion);
+  return( completion.choices[0].message.content);
+}
+//ChatGPT 끝
 
 // remote kakao 설정 시작
-const prefix = ">";
+const prefix : string = "!";
 const server = new UDPServer({ serviceName: "Example Service" });
 
 
@@ -13,7 +26,7 @@ server.on("message", async (msg) => {
     if (!msg.content.startsWith(prefix)) return;
   
     const args = msg.content.split(" ");
-    const cmd = args.shift()?.slice(prefix.length);
+    const cmd  = args.shift()?.slice(prefix.length);
   
     if (cmd === "ping") {
       /*
@@ -23,6 +36,25 @@ server.on("message", async (msg) => {
       const timestamp = Date.now();
       await msg.replyText("Pong!");
       msg.replyText(`${Date.now() - timestamp}ms`);
+    } else if (cmd === "gpt"){
+      const p_role = "system";
+      let p_content= "";
+      
+      if (args.length >=1){
+        p_content = msg.content.split(prefix.concat(cmd))[1];
+
+        const rep: any = await gpt_question(p_role,p_content);
+        console.log(msg);
+
+        console.log(rep);
+        
+        msg.replyText(msg.sender.name + "님의 질문\nQ. " + p_content + "\n\n"+  rep);
+      }
+      else {
+        msg.replyText("질문이 없습니다.");
+      }
+      
+      
     }
 });
   
@@ -30,8 +62,6 @@ server.start();
 
 
 // remote kakao 설정 끝
-
-
 
 /*
 app.get('/', (req: Request, res: Response) => {
